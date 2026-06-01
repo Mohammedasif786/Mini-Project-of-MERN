f90c554a58e4cc1348fb6d89efdc5cb9a3cdfcaa
Mohammed Asif <formohammedasif6@gmail.com>
2026-06-01 13:42:49 +0530

    Checking done

--- LapStopWatchSimple/src/App.tsx
+++ LapStopWatchSimple/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Stopwatch2 from "./components/Stopwatch2";
 import { TimeData, type LapEntry, type TimeFomartDisplay } from "./context/timerHandling";
 
@@ -16,7 +16,27 @@ function App() {
   const [reset, setReset] = useState<boolean>(false);
   const [Edits,setEdit] = useState<boolean>(false);
 
-  const allWorkingData = {
+  // const allWorkingData = {
+  //   Edits,
+  //   setEdit,
+  //   elapsed,
+  //   setElapsed,
+  //   isRunning,
+  //   setIsRunning,
+  //   laps,
+  //   setLaps,
+  //   startTime,
+  //   setStartTime,
+  //   intervalId,
+  //   setIntervalId,
+  //   startEngine,
+  //   setStartEngine,
+  //   reset,
+  //   setReset,
+  // };
+
+  const allWorkingData = useMemo(
+  () => ({
     Edits,
     setEdit,
     elapsed,
@@ -33,8 +53,18 @@ function App() {
     setStartEngine,
     reset,
     setReset,
-  };
-
+  }),
+  [
+    Edits,
+    elapsed,
+    isRunning,
+    laps,
+    startTime,
+    intervalId,
+    startEngine,
+    reset,
+  ]
+);
   return (
     <TimeData.Provider value={allWorkingData}>
       <Stopwatch2 />
